cfdca737a76b2e0b9551dc33044d29719bdf4dfd
nirvanich <nirvanich@gmail.com>
2016-07-05 12:20:10 +0300

    Application.java was added

---
+++ src/main/java/com/crx/uitests/requirements/Application.java
@@ -0,0 +1,11 @@
+package com.crx.uitests.requirements;
+
+import net.thucydides.core.annotations.Feature;
+
+public class Application {
+    @Feature
+    public class Jira {
+        public class IssueManagement {
+        }
+    }
+}
